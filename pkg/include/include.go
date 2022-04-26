9bfaf664d206c000852a35e5cc643ff12e70770e
ethfoo <ethfoo@163.com>
2022-04-26 10:40:01 +0800

    Feat: add sls sink; rename log.proto to loggie.proto

--- pkg/include/include.go
+++ pkg/include/include.go
@@ -44,6 +44,7 @@ import (
 	_ "github.com/loggie-io/loggie/pkg/sink/grpc"
 	_ "github.com/loggie-io/loggie/pkg/sink/kafka"
 	_ "github.com/loggie-io/loggie/pkg/sink/loki"
+	_ "github.com/loggie-io/loggie/pkg/sink/sls"
 	_ "github.com/loggie-io/loggie/pkg/source/dev"
 	_ "github.com/loggie-io/loggie/pkg/source/file"
 	_ "github.com/loggie-io/loggie/pkg/source/file/process"
